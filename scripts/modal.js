fbdf0f53c9cb7802cc45b888726713986420ebf3
suenefuzzo <suenequeiroz@hotmail.com>
2022-09-29 22:01:35 -0300

    js-modal

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,1 +1,8 @@
-/* Desenvolva sua lógica aqui */
+const buttonModal = document.querySelectorAll("[data-modal-control]")
+
+buttonModal.forEach((element) => {
+    element.addEventListener('click', () => {
+        let valueModal = element.getAttribute("data-modal-control")
+        document.getElementById(valueModal).classList.toggle("show-modal")
+    })
+});
